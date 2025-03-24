f3a43dd949b437790b8631c91bb232cd4cfbd42f
Student pranjalsihag6104 <pranjalsihag6104@gmail.com>
2025-03-25 02:28:37 +0530

    error

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express=require('express');
 const path=require('path');
 const mongoose=require('mongoose');
 const ejsMate=require('ejs-mate');
-const catchAsync=require('./utils/catchAsync')
+const catchAsync=require('./utils/catchAsync');
+const ExpressError=require('./utils/ExpressError');
 const methodOverride=require('method-override');
 const Campground=require('./models/campground');
 
@@ -40,7 +41,7 @@ app.get('/campgrounds/new',(req,res)=>{
 });
 
 app.post('/campgrounds',catchAsync(async(req,res,next)=>{
-  
+  if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
   const campground=new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`)
@@ -69,9 +70,13 @@ app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
   res.redirect('/campgrounds');
 }));
 
-
+app.all('*',(req,res,next)=>{
+  next(new ExpressError('page not found',404))
+})
 
 app.use((err,req,res,next)=>{
+  const {statusCode=500,message='something went wrong'}=err;
+  res.status(statusCode).send(message);
   res.send("something went wrong");
 
 });
