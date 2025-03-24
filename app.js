b0c1ee1179ed9cea9f2be4fda18823a0892c7a65
Student pranjalsihag6104 <pranjalsihag6104@gmail.com>
2025-03-25 05:11:36 +0530

    routes

--- app.js
+++ app.js
@@ -74,7 +74,8 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 }));
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id)
+  const campground = await Campground.findById(req.params.id).populate('reviews');
+  
   res.render('campgrounds/show', { campground });
 }));
 
@@ -102,6 +103,13 @@ app.post('/campgrounds/:id/reviews',validateReview,  catchAsync(async (req, res)
   await review.save();
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId',catchAsync(async(req,res)=>{
+  const {id,reviewId}=req.params;
+  await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+   res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all('*', (req, res, next) => {
