fd29dae44419f3c6039703368d30a65cd8ba703c
Student pranjalsihag6104 <pranjalsihag6104@gmail.com>
2025-03-27 02:30:19 +0530

    flash

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const { campgroundSchema,reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
+const session =require('express-session');
+const flash=require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
@@ -11,10 +13,12 @@ const methodOverride = require('method-override');
 const campgrounds=require('./routes/campgrounds');
 const reviews=require('./routes/reviews');
 
-  mongoose.connect('mongodb://127.0.0.1:27017/new-yelp-camp-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+mongoose.connect('mongodb://127.0.0.1:27017/new-yelp-camp-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  
+})
+  
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connnection error:"));
@@ -32,6 +36,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')))
 
+const sessionConfig={
+  secret:'thishouldbeasecret',
+  resave:false,
+  saveuninitialised:true,
+  cookie:{
+    httpOnly:true,    
+    expires:Date.now()+1000*60*60*24*7,
+    maxAge:1000*60*60*24*7
+  }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req,res,next)=>{
+  res.locals.success=req.flash('success');
+  res.locals.error=req.flash('error');
+  next();
+})
 
 app.use('/campgrounds',campgrounds)
 app.use('/campgrounds/:id/reviews',reviews)
