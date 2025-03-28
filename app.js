80095f325a0174b99db350a6bc5f3d7aa40c5cc8
Student pranjalsihag6104 <pranjalsihag6104@gmail.com>
2025-03-28 23:40:30 +0530

    deploy

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
   require('dotenv').config();
 }
 
-console.log(process.env.SECRET); 
+console.log(process.env.SECRET);
 
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { campgroundSchema,reviewSchema } = require('./schemas.js');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
-const session =require('express-session');
-const flash=require('connect-flash');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const passport=require('passport');
-const LocalStrategy=require('passport-local');
-const User=require('./models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const userRoutes=require('./routes/users');
-const campgroundRoutes=require('./routes/campgrounds');
-const reviewRoutes=require('./routes/reviews');
+const mongoSanitize = require('express-mongo-sanitize');
+
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://127.0.0.1:27017/new-yelp-camp-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  
+
 })
-  
+
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connnection error:"));
@@ -43,16 +45,17 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
-const sessionConfig={
-  secret:'thishouldbeasecret',
-  resave:false,
-  saveuninitialised:true,
-  cookie:{
-    httpOnly:true,    
-    expires:Date.now()+1000*60*60*24*7,
-    maxAge:1000*60*60*24*7
+
+const sessionConfig = {
+  secret: 'thishouldbeasecret',
+  resave: false,
+  saveuninitialised: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
   }
 }
 app.use(session(sessionConfig));
@@ -65,24 +68,24 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   //console.log(req.session)
-  res.locals.currentUser=req.user;
-  res.locals.success=req.flash('success');
-  res.locals.error=req.flash('error');
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
   next();
 })
 
-app.get('/fakeUser',async(req,res)=>{
-  const user=new User({email:'hi@gmail.com',username:'coltt'});
-  const newUser=await User.register(user,'chicken');
-  res.send(newUser); 
+app.get('/fakeUser', async (req, res) => {
+  const user = new User({ email: 'hi@gmail.com', username: 'coltt' });
+  const newUser = await User.register(user, 'chicken');
+  res.send(newUser);
 
 })
 
-app.use('/',userRoutes); 
-app.use('/campgrounds',campgroundRoutes)
-app.use('/campgrounds/:id/reviews',reviewRoutes)
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -114,6 +117,6 @@ app.all('*', (req, res, next) => {
 });
 
 
-app.listen(3001, () => {
+app.listen(process.env.PORT|| 3001, () => {
   console.log("serving on port 3000")
 })
